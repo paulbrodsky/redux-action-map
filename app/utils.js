a64c58cf5cd66948872b1552f8061b185b3589ef
Paul Brodsky <paul.brodsky@gmail.com>
2016-06-26 15:11:20 -0400

    refactor

--- app/utils.js
+++ app/utils.js
@@ -35,27 +35,6 @@ export default function createReducer(mutator) {
   };
 }
 
-// export default function createReducer(mutator) {
-//   const combinedState = getCombinedState(mutator);
-//   return (state = combinedState, action) => {
-//     if (isUndefined(state) || isUndefined(action)) {
-//       return;
-//     }
-//     if (mutator.type === action.type) {
-//       state = executeMutator(mutator, state, action);
-//     }
-//     if (mutator.mutators && mutator.mutators.length > 0) {
-//       forEach(mutator.mutators, m => {
-//         if (m.type === action.type) {
-//           state = executeMutator(m, state, action);
-//           state = executeMutator(mutator, state, action);
-//         }
-//       });
-//     }
-//     return state;
-//   };
-// }
-
 export function createCommand(dispatch, type) {
   return (payload) => dispatch({ type, payload });
 }
